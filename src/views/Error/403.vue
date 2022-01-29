64adfe0b80be4bf457e0b21ea9091b3c174fbeb8
就眠儀式 <854085467@qq.com>
2022-01-30 00:39:06 +0800

    更新 layui-vue 0.3.6-alpha.6

--- src/views/Error/403.vue
+++ src/views/Error/403.vue
@@ -1,5 +1,10 @@
 <template>
-    <lay-card>
-        403
+    <lay-card style="padding-top:200px;padding-bottom:200px;">
+        <lay-exception status="403" title="403">
+            <template #action>
+                <lay-button>刷新</lay-button>
+                <lay-button type="primary">返回</lay-button>
+            </template>
+        </lay-exception>
     </lay-card>
 </template>
--- src/views/Error/500.vue
+++ src/views/Error/500.vue
@@ -1,5 +1,10 @@
 <template>
-    <lay-card>
-        500
+    <lay-card style="padding-top:200px;padding-bottom:200px;">
+        <lay-exception status="500" title="500">
+            <template #action>
+                <lay-button>刷新</lay-button>
+                <lay-button type="primary">返回</lay-button>
+            </template>
+        </lay-exception>
     </lay-card>
 </template>
