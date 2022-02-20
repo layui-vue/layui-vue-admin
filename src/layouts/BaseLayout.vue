50c5407201eef98c3e8ae29df2077062dc7515fe
就眠儀式 <854085467@qq.com>
2022-02-21 01:19:53 +0800

    升级 layui-vue

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,102 +1,104 @@
 <template>
   <lay-config-provider :themeVariable="themeVariable">
     <lay-layout :class="[collapseState ? 'collapse' : '']">
-    <!-- side -->
-    <lay-side :black="isBlack">
-      <lay-logo> </lay-logo>
-      <lay-menu
-        v-model:selectedKey="selectKey"
-        v-model:openKeys="openKeys"
-        :tree="isTree"
-      >
-        <lay-sub-menu title="工作空间" id="0">
-          <lay-menu-item id="/console"> 控制台 </lay-menu-item>
-          <lay-menu-item id="/analysis"> 分析页 </lay-menu-item>
-        </lay-sub-menu>
-        <lay-sub-menu title="错误页面" id="1">
-          <lay-menu-item id="/error/401"> 401 </lay-menu-item>
-          <lay-menu-item id="/error/403"> 403 </lay-menu-item>
-          <lay-menu-item id="/error/404"> 404 </lay-menu-item>
-          <lay-menu-item id="/error/500"> 500 </lay-menu-item>
-        </lay-sub-menu>
-        <lay-sub-menu title="系统管理" id="2">
-          <lay-menu-item id="/system/user"> 用户管理 </lay-menu-item>
-          <lay-menu-item id="/system/role"> 角色管理 </lay-menu-item>
-        </lay-sub-menu>
-        <lay-sub-menu title="结果页面" id="3">
-          <lay-menu-item id="/result/success"> 成功页面 </lay-menu-item>
-          <lay-menu-item id="/result/failure"> 失败页面 </lay-menu-item>
-        </lay-sub-menu>
-      </lay-menu>
-    </lay-side>
-    <lay-layout>
-      <!-- header -->
-      <lay-header>
-        <lay-menu class="layui-layout-left">
-          <lay-menu-item @click="collapse">
-            <lay-icon
-              v-if="collapseState"
-              type="layui-icon-spread-left"
-            ></lay-icon>
-            <lay-icon v-else type="layui-icon-shrink-right"></lay-icon>
-          </lay-menu-item>
-          <lay-menu-item @click="refresh">
-            <lay-icon type="layui-icon-refresh-one"></lay-icon>
-          </lay-menu-item>
-        </lay-menu>
-        <lay-menu class="layui-layout-right">
-          <lay-dropdown>
-            <lay-menu-item>
-              <lay-icon type="layui-icon-notice"></lay-icon>
-            </lay-menu-item>
-            <template #content>
-                内容
-            </template>
-          </lay-dropdown>
-          <lay-dropdown>
-            <lay-menu-item>
-              <lay-icon type="layui-icon-username"></lay-icon>
-            </lay-menu-item>
-            <template #content>
-              <lay-dropdown-item>用户信息</lay-dropdown-item>
-              <lay-dropdown-item>注销登录</lay-dropdown-item>
-            </template>
-          </lay-dropdown>
-
-          <div style="position: relative;display: inline-block;">
-            <lay-menu-item @click="changeVisible">
-              <lay-icon type="layui-icon-more-vertical"></lay-icon>
-            </lay-menu-item>
-          </div>
-
-        </lay-menu>
-      </lay-header>
-      <!-- content -->
-      <lay-body>
-        <lay-tab
-          :modelValue="route.path"
-          @change="change"
-          :allowClose="allowClose"
-          @close="close"
+      <!-- side -->
+      <lay-side :black="isBlack">
+        <lay-logo> </lay-logo>
+        <lay-menu
+          v-model:selectedKey="selectKey"
+          v-model:openKeys="openKeys"
+          :tree="isTree"
         >
-          <lay-tab-item
-            v-for="tab in tabs"
-            :key="tab"
-            :title="tab.title"
-            :id="tab.id"
-            :closable="tab.closable"
-          ></lay-tab-item>
-        </lay-tab>
-        <router-view v-if="isRouterAlive"></router-view>
-      </lay-body>
-      <lay-footer></lay-footer>
+          <lay-sub-menu title="工作空间" id="0">
+            <lay-menu-item id="/console"> 控制台 </lay-menu-item>
+            <lay-menu-item id="/analysis"> 分析页 </lay-menu-item>
+          </lay-sub-menu>
+          <lay-sub-menu title="错误页面" id="1">
+            <lay-menu-item id="/error/401"> 401 </lay-menu-item>
+            <lay-menu-item id="/error/403"> 403 </lay-menu-item>
+            <lay-menu-item id="/error/404"> 404 </lay-menu-item>
+            <lay-menu-item id="/error/500"> 500 </lay-menu-item>
+          </lay-sub-menu>
+          <lay-sub-menu title="系统管理" id="2">
+            <lay-menu-item id="/system/user"> 用户管理 </lay-menu-item>
+            <lay-menu-item id="/system/role"> 角色管理 </lay-menu-item>
+          </lay-sub-menu>
+          <lay-sub-menu title="结果页面" id="3">
+            <lay-menu-item id="/result/success"> 成功页面 </lay-menu-item>
+            <lay-menu-item id="/result/failure"> 失败页面 </lay-menu-item>
+          </lay-sub-menu>
+        </lay-menu>
+      </lay-side>
+      <lay-layout>
+        <!-- header -->
+        <lay-header>
+          <lay-menu class="layui-layout-left">
+            <lay-menu-item @click="collapse">
+              <lay-icon
+                v-if="collapseState"
+                type="layui-icon-spread-left"
+              ></lay-icon>
+              <lay-icon v-else type="layui-icon-shrink-right"></lay-icon>
+            </lay-menu-item>
+            <lay-menu-item @click="refresh">
+              <lay-icon type="layui-icon-refresh-one"></lay-icon>
+            </lay-menu-item>
+          </lay-menu>
+          <lay-menu class="layui-layout-right">
+            <lay-dropdown>
+              <lay-menu-item>
+                <lay-icon type="layui-icon-notice"></lay-icon>
+              </lay-menu-item>
+              <template #content> 内容 </template>
+            </lay-dropdown>
+            <lay-dropdown>
+              <lay-menu-item>
+                <lay-icon type="layui-icon-username"></lay-icon>
+              </lay-menu-item>
+              <template #content>
+                <lay-dropdown-item>用户信息</lay-dropdown-item>
+                <lay-dropdown-item>注销登录</lay-dropdown-item>
+              </template>
+            </lay-dropdown>
+
+            <div style="position: relative; display: inline-block">
+              <lay-menu-item @click="changeVisible">
+                <lay-icon type="layui-icon-more-vertical"></lay-icon>
+              </lay-menu-item>
+            </div>
+          </lay-menu>
+        </lay-header>
+        <!-- content -->
+        <lay-body>
+          <lay-tab
+            :modelValue="route.path"
+            @change="change"
+            :allowClose="allowClose"
+            @close="close"
+          >
+            <lay-tab-item
+              v-for="tab in tabs"
+              :key="tab"
+              :title="tab.title"
+              :id="tab.id"
+              :closable="tab.closable"
+            ></lay-tab-item>
+          </lay-tab>
+          <router-view v-if="isRouterAlive"></router-view>
+        </lay-body>
+        <lay-footer></lay-footer>
+      </lay-layout>
     </lay-layout>
-  </lay-layout>
-  <lay-layer title="更多设置" type="drawer" v-model="visible"></lay-layer>
+    <lay-layer title="更多设置" type="drawer" area="360px" v-model="visible">
+      {{ themeVariable }}
+      <lay-color-picker
+        v-model="themeVariable['--global-primary-color']"
+      ></lay-color-picker>
+    </lay-layer>
   </lay-config-provider>
 </template>
 <script>
-import { ref, watch } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 export default {
   setup() {
@@ -115,18 +117,16 @@ export default {
     const visible = ref(false);
     const tabs = ref([{ title: "首页", id: "/console", closable: false }]);
 
-    const changeVisible = function() {
-      visible.value = !visible.value
-    }
+    const changeVisible = function () {
+      visible.value = !visible.value;
+    };
 
     watch(selectKey, () => {
       router.push(selectKey.value);
     });
-    
+
     const change = function (id) {
       selectKey.value = id;
-
-      alert(id);
       router.push(id);
     };
 
@@ -161,10 +161,15 @@ export default {
       }
     });
 
-    const themeVariable = {
-      "--global-primary-color":"#2d8cf0",
-      "--global-checked-color":"#2d8cf0"
-    }
+    const themeVariable = reactive({
+      "--global-primary-color": "#1e9fff",
+      "--global-normal-color": "#1e9fff",
+      "--global-warm-color": "#ffb800",
+      "--global-danger-color": "#ff5722",
+      "--global-checked-color": "#5fb878",
+      "--global-border-radius": "2px",
+    });
+
     // return instance
     return {
       changeVisible,
