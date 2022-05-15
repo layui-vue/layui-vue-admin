348b4c1b63fddbd58d35be4b44c52298b0f143b8
就眠儀式 <854085467@qq.com>
2022-05-15 16:47:18 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { LayuiVueResolver } from 'unplugin-layui-vue-resolver'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { LayuiVueResolver } from "unplugin-layui-vue-resolver";
 
 export default defineConfig({
   plugins: [
-  vue(),
-  // 按需插件
-  AutoImport({
-    resolvers: [LayuiVueResolver({
-      resolveIcons: true
-    })],
-  }),
-  Components({
-    resolvers: [LayuiVueResolver({
-      resolveIcons: true
-    })],
-  }),]
-})
+    // 按需插件
+    AutoImport({
+      resolvers: [
+        LayuiVueResolver(),
+      ],
+    }),
+    Components({
+      resolvers: [
+        LayuiVueResolver(),
+      ],
+    }),
+    vue(),
+  ],
+});
